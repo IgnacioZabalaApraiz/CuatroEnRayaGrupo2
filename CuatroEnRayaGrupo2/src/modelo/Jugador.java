0d84b568797b5241138b2975034ae4f9ec7797a5
Noxhile <Noxhile@DESKTOP-M206G78>
2024-04-03 19:21:43 +0200

    Commit actualización clase Jugador:
    - Atributos numVictorias,numEmpates,numDerrotas

--- CuatroEnRayaGrupo2/src/modelo/Jugador.java
+++ CuatroEnRayaGrupo2/src/modelo/Jugador.java
@@ -3,37 +3,46 @@ package modelo;
 public class Jugador implements Comparable<Jugador> {
 	private String nombre;
 	private String color;
+	private int numVictorias;
+	private int numEmpates;
+	private int numDerrotas;
 	private int puntuacion = 0;
 
-	
 	public Jugador(String nombre, String color) {
 		this.nombre = nombre;
 		this.color = color;
 	}
 
-	// Método para que la puntuación incremente si el jugador ha ganado
-	public void haTerminado(boolean haGanado) {
-		if (haGanado) {
-			this.puntuacion += 3;
-		} else {
-			this.puntuacion += 1;
+	// Método para registrar el resultado de un juego
+	public void registrarResultado(int resultado) {
+		if (resultado == 1) { // Victoria
+			numVictorias++;
+		} else if (resultado == 0) { // Empate
+			numEmpates++;
+		} else { // Derrota
+			numDerrotas++;
 		}
+	}
+
+	// Método para calcular la puntuación del jugador
+	public int calcularPuntuacion() {
+		return numVictorias * 3 + numEmpates;
 	}
 
 	// Método para obtener la puntuación
 	public int getPuntuacion() {
-		return puntuacion;
+		return calcularPuntuacion();
 	}
 
 	// Método para mostrar el nombre y la puntuación del jugador
 	@Override
 	public String toString() {
-		return nombre + ": " + puntuacion;
+		return nombre + ": " + calcularPuntuacion();
 	}
 
 	// Método para comparar las puntuaciones de los jugadores
 	@Override
 	public int compareTo(Jugador otroJugador) {
-		return Integer.compare(this.puntuacion, otroJugador.getPuntuacion());
+		return Integer.compare(this.calcularPuntuacion(), otroJugador.calcularPuntuacion());
 	}
 }
